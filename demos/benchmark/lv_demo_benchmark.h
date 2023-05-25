639de671cbdf21112123d0c07e6d602a4a9d3e67
HanRabbit <tianzihan5357@163.com>
2023-05-25 14:36:17 +0800

    first commit

---
+++ demos/benchmark/lv_demo_benchmark.h
@@ -0,0 +1,42 @@
+/**
+ * @file lv_demo_benchmark.h
+ *
+ */
+
+#ifndef LV_DEMO_BENCHMARK_H
+#define LV_DEMO_BENCHMARK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdint.h>
+#include "../lv_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_demo_benchmark(void);
+
+void lv_demo_benchmark_run_scene(uint16_t scene_no);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_DEMO_BENCHMARK_H*/
